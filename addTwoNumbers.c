ba0c14ba69e87baca1579db6e53d8e572db96b5e
YiCheng-Li <yli4823@uwo.ca>
2026-01-16 21:25:34 -0500

    nothing

---
+++ addTwoNumbers.c
@@ -0,0 +1,11 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
+    return l1;
+}
